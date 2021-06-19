b879f8a1a4dac971d9d0db79bcb2b574b0dc4169
Rami <RamChilla@users.noreply.github.com>
2021-06-19 00:24:25 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-app :style="{background: $vuetify.theme.themes['light'].background}">
+  <v-app :style="{ background: $vuetify.theme.themes['light'].background }">
     <Navbar :appName="appName" />
-    <v-content>
+    <v-main>
       <router-view></router-view>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 export default {
-  name: 'App',
+  name: "App",
   components: { Navbar },
   data: () => ({
-    appName: 'bingo picker'
-  })
+    appName: "bingo picker",
+  }),
 };
 </script>
